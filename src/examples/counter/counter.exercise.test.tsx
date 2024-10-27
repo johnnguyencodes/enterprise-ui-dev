86c6506d6039df1ea60cf231a22b1849823d4671
John Nguyen <jnguyen92780@gmail.com>
2024-10-27 10:15:39 -0700

    refactor

--- src/examples/counter/counter.exercise.test.tsx
+++ src/examples/counter/counter.exercise.test.tsx
@@ -3,17 +3,23 @@
 import Counter from '.';
 import { render } from './test/utilities';
 
+const setup = (initialCount = 0) => {
+  const { user, getByRole, getByTestId } = render(
+    <Counter initialCount={initialCount} />,
+  );
+  const incrementButton = getByRole('button', { name: /increment/i });
+  const resetButton = getByRole('button', { name: /reset/i });
+  const currentCount = getByTestId('current-count');
+  return { user, incrementButton, resetButton, currentCount };
+};
+
 test('it should render the component', () => {
   const { debug } = render(<Counter />);
   debug();
 });
 
 test('should increment when the increment button is pressed', async () => {
-  const { user, getByTestId, getByRole } = render(<Counter />);
-
-  // Get the current count element and the increment button
-  const currentCount = getByTestId('current-count');
-  const incrementButton = getByRole('button', { name: /Increment/i });
+  const { user, currentCount, incrementButton } = setup(0);
 
   // Verify initial count is 0
   expect(currentCount).toHaveTextContent('0');
@@ -26,24 +32,20 @@ test('should increment when the increment button is pressed', async () => {
 });
 
 test('it should render the component with an initial count', () => {
-  // render component and set initial count
-  const { getByTestId } = render(<Counter initialCount={4000} />);
+  const { currentCount } = setup(4000);
 
-  expect(getByTestId('current-count')).toHaveTextContent('4000');
+  expect(currentCount).toHaveTextContent('4000');
 });
 
 test('it should reset the count when the "Reset" button is pressed', async () => {
-  // render component and pull out user
-  const { user, getByRole, getByTestId } = render(
-    <Counter initialCount={4000} />,
-  );
+  const { user, currentCount, resetButton } = setup(4000);
 
   // Verify initial count is 4000
-  expect(getByTestId('current-count')).toHaveTextContent('4000');
+  expect(currentCount).toHaveTextContent('4000');
 
   // Click the reset button
-  await user.click(getByRole('button', { name: /Reset/i }));
+  await user.click(resetButton);
 
   // Verify the count is now 0
-  expect(getByTestId('current-count')).toHaveTextContent('0');
+  expect(currentCount).toHaveTextContent('0');
 });
