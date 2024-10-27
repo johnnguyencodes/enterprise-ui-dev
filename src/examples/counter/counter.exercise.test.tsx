640a664e4fe8d304d586bdb88d44dc9fa6c16c84
John Nguyen <jnguyen92780@gmail.com>
2024-10-27 09:58:55 -0700

    refactor

--- src/examples/counter/counter.exercise.test.tsx
+++ src/examples/counter/counter.exercise.test.tsx
@@ -1,20 +1,19 @@
 // @vitest-environment happy-dom
 
-import { screen } from '@testing-library/react';
 import Counter from '.';
 import { render } from './test/utilities';
 
 test('it should render the component', () => {
-  render(<Counter />);
-  screen.debug();
+  const { debug } = render(<Counter />);
+  debug();
 });
 
 test('should increment when the increment button is pressed', async () => {
-  const { user } = render(<Counter />);
+  const { user, getByTestId, getByRole } = render(<Counter />);
 
   // Get the current count element and the increment button
-  const currentCount = screen.getByTestId('current-count');
-  const incrementButton = screen.getByRole('button', { name: /Increment/i });
+  const currentCount = getByTestId('current-count');
+  const incrementButton = getByRole('button', { name: /Increment/i });
 
   // Verify initial count is 0
   expect(currentCount).toHaveTextContent('0');
