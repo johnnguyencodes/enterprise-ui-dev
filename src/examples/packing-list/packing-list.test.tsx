b4a1e92188e8f40a590927dd678bf351bb5603ea
John Nguyen <jnguyen92780@gmail.com>
2024-10-27 19:14:40 -0700

    refactor

--- src/examples/packing-list/packing-list.test.tsx
+++ src/examples/packing-list/packing-list.test.tsx
@@ -1,18 +1,19 @@
 // @vitest-environment jsdom
 
 import PackingList from '.';
-import { render, screen } from 'test/utilities';
+import { render, screen, waitFor } from 'test/utilities';
 
 const renderPackingList = () => {
   const { user, getByText, getByLabelText, getByRole, getByTestId } = render(
     <PackingList />,
   );
-  const h1Title = getByText('Packing List');
 
-  const inputField = getByLabelText('New Item Name');
-  const addButton = getByRole('button', { name: /add new item/i });
+  const h1Title = getByText('Packing List');
+  const newItemInput = getByLabelText('New Item Name');
+  const addNewItemButton = getByRole('button', { name: /add new item/i });
   const unpackedList = getByTestId('unpacked-items-list');
-  return { user, h1Title, inputField, addButton, unpackedList };
+
+  return { user, h1Title, newItemInput, addNewItemButton, unpackedList };
 };
 
 it('renders the Packing List application', () => {
@@ -26,48 +27,52 @@ it('has the correct title', () => {
 });
 
 it('has an input field for a new item', () => {
-  const { inputField } = renderPackingList();
-  expect(inputField).toHaveValue('');
+  const { newItemInput } = renderPackingList();
+  expect(newItemInput).toHaveValue('');
 });
 
 it('has a "Add New Item" button that is disabled when the input is empty', async () => {
-  const { inputField, addButton } = renderPackingList();
-  expect(inputField).toHaveValue('');
-  expect(addButton).toBeDisabled();
+  const { newItemInput, addNewItemButton } = renderPackingList();
+  expect(newItemInput).toHaveValue('');
+  expect(addNewItemButton).toBeDisabled();
 });
 
 it('enables the "Add New Item" button when there is text in the input field', async () => {
-  const { user, inputField, addButton } = renderPackingList();
-  expect(inputField).toHaveValue('');
-  expect(addButton).toBeDisabled();
+  const { user, newItemInput, addNewItemButton } = renderPackingList();
+  expect(newItemInput).toHaveValue('');
+  expect(addNewItemButton).toBeDisabled();
 
-  await user.type(inputField, 'Macbook Pro');
-  expect(inputField).toHaveValue('Macbook Pro');
+  await user.type(newItemInput, 'iPhone');
+  expect(newItemInput).toHaveValue('iPhone');
 
-  expect(addButton).not.toBeDisabled();
+  expect(addNewItemButton).not.toBeDisabled();
 });
 
 it('adds a new item to the unpacked item list when the clicking "Add New Item"', async () => {
-  const { user, inputField, addButton, unpackedList } = renderPackingList();
+  const { user, newItemInput, addNewItemButton, unpackedList } =
+    renderPackingList();
 
-  await user.type(inputField, 'Macbook Pro');
-  expect(inputField).toHaveValue('Macbook Pro');
+  await user.type(newItemInput, 'Apple Watch');
+  expect(newItemInput).toHaveValue('Apple Watch');
 
-  await user.click(addButton);
-  expect(unpackedList).toHaveTextContent('Macbook Pro');
-  expect(inputField).toHaveValue('');
+  await user.click(addNewItemButton);
+  expect(unpackedList).toHaveTextContent('Apple Watch');
+  expect(screen.getByLabelText('Apple Watch')).not.toBeChecked();
+  expect(newItemInput).toHaveValue('');
+  expect(addNewItemButton).toBeDisabled();
 });
 
 it('removes the item from the unpacked item list after it has been added', async () => {
-  const { user, inputField, addButton, unpackedList } = renderPackingList();
+  const { user, newItemInput, addNewItemButton, unpackedList } =
+    renderPackingList();
 
-  await user.type(inputField, 'iPad');
-  await user.click(addButton);
+  await user.type(newItemInput, 'iPad');
+  await user.click(addNewItemButton);
 
-  const removeButton = screen.getByRole('button', {
+  const removeItemButton = screen.getByRole('button', {
     name: /remove ipad/i,
   });
-  await user.click(removeButton);
+  await user.click(removeItemButton);
 
-  expect(unpackedList).not.toHaveTextContent('iPad');
+  await waitFor(() => expect(unpackedList).not.toHaveTextContent('iPad'));
 });
